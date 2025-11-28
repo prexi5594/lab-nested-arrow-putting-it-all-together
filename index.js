e6f88bf0bdfdd61e35c535938bd330a27c7d549a
Prexidazie Morara <moraraprexidazie@gmail.com>
2025-11-28 20:28:59 +0300

    completed JS functions lab

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-function createLoginTracker(username) {}
-  let attempts = 0;
-  const  maxAttempts = 3;
+function createLoginTracker(userInfo) {
+    let attempts = 0;
+    const maxAttempts = 3;
 
-  return (username , password) => {
-    if (attempts <= maxAttempts ){
-      return "Account locked too many failed login attempts.";
-    }
-  }
+    return (username, password) => {
+        if (attempts >= maxAttempts) {
+            return "Account locked. Too many failed attempts.";
+        }
 
-  if (username === username && password === username.password) {
-    return " login successful.";
-  } else {
-    attempts++;
-    if (attempts >= maxAttempts) {
-      return "Account locked too many failed login attempts.";
-  }
-  }   return "incorrect login. Attempts left: ${maxAttempts - attempts} "; 
+        if (username === userInfo.username && password === userInfo.password) {
+            return "Login successful!";
+        } else {
+            attempts++;
+            if (attempts >= maxAttempts) {
+                return "Account locked. Too many failed attempts.";
+            }
+            return `Incorrect login. Attempts left: ${maxAttempts - attempts}`;
+        }
+    };
+}
 
-
-  // example test 
-
-  const username = { username: "userA", password: "5594" }
-  const login = createLoginTracker(username);
-
-
-  console.log(login("userA", "wrongPassword")); // incorrect login. attempts left:2
-  console.log(login("userA", "wrongPassword")); // incorrect login. attempts left:1
-  console.log(login("userA", "wrongPassword")); // Account locked too many failed login attempts.
-  console.log(login("userA", "5594")); // Account locked too many failed login attempts.
-  
-  
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+module.exports = { createLoginTracker };
